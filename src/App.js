50226c12e8281c2ac81d2c279a12478d4edb228d
Ivan <inovosyolov2000@gmail.com>
2023-01-13 23:58:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { randomColor } from 'randomcolor'
+import Draggable from 'react-draggable'
+import './App.css'
 
 function App() {
+
+  const [item, setItem] = useState('')
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items])
+
+  const newItem = () => {
+    if (item.trim() !== '') {
+      const newItem = {
+        id: uuidv4(),
+        item,
+        color: randomColor({
+          luminosity: 'light'
+        }),
+        defaultPos: {
+          x: 500,
+          y: -500
+        }
+      }
+      setItems((items) => [...items, newItem])
+      setItem('')
+    }
+    else {
+      alert('Enter something...')
+      setItem('')
+    }
+  }
+
+  const deleteNode = (id) => {
+    setItems(items.filter((item) => item.id !== id ))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <input 
+        placeholder='Enter something...'
+        type="text" 
+        onChange={ (e) => setItem(e.target.value) }
+        />
+        <button className="enter-btn" onClick={newItem}>Enter</button>
+      </div>
+
+      {
+        items.map((item, index) => {
+          return (
+            <Draggable 
+              key={index}
+              defaultPosition={item.defaultPos}
+            >
+              
+              <div className="todo__item" style={{backgroundColor: item.color}}>
+                {`${item.item}`}
+                <button 
+                className='delete-btn'
+                onClick={() => deleteNode(item.id)}
+                >
+                  X
+                </button>
+              </div>
+
+            </Draggable>
+          )
+        })
+      }
+
     </div>
   );
 }
